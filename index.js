15835a48731e4b3fe90d8b96fdb4485bf3ca3b70
Paulhan14 <pauljasonhan@gmail.com>
2024-02-01 16:51:25 -0700

    try more

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ require("./routes/followingRoutes")(app);
 require("./routes/articleRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("frontend/build"));
+	app.use(express.static("frontend/build/"));
 	const path = require("path");
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
