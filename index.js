fd75a46632ac6b12825d3e514db5ef24c97e1a48
Paulhan14 <pauljasonhan@gmail.com>
2024-02-01 17:14:25 -0700

    define route specifically

--- index.js
+++ index.js
@@ -51,6 +51,16 @@ require("./routes/articleRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("frontend/build/"));
+	app.get("/", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "frontend", "build", "index.html")
+		);
+	});
+	app.get("/favicon.ico", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "frontend", "build", "favicon.ico")
+		);
+	});
 }
 
 // Get the port from the environment, i.e., Heroku sets it
